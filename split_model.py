992a7fe9ffd151c09388309ebc42881eddfe1bd8
SofiaRaak <sofia.raak@bristol.ac.uk>
2022-02-03 10:21:51 +0000

    Introduced split model
    Added a .py file of a model where wt and short mirna are individually
    calculated
    
    Changed k**2 to np.log(k) in both split_model and model

--- split_model.py
+++ split_model.py
@@ -1,5 +1,3 @@
-## setup environment
-
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import minimize
@@ -7,7 +5,6 @@ from scipy.optimize import curve_fit
 import cma
 import os
 
-##setup initial values for model
 #timesteps
 dt = 0.0001
 minutes = 60
@@ -38,13 +35,10 @@ short_y = np.array([0, 0.0033684107002276975, 0.007599822974028003, 0.0100191778
 data_values = np.array([[WT_y],
                        [short_y]])
 
-##create tester function to ensure model module loaded
-def test():
-    print("Module model.py successfully loaded.")
-
-##model
+#split model
 def conc_change(theta):    
     k1, k2, k3 = theta
+    
     k1 = np.log(k1)
     k2 = np.log(k2)
     k3 = np.log(k3)
@@ -52,43 +46,52 @@ def conc_change(theta):
     #empty arrays
     WT = np.zeros(int(minutes/dt))
     short = np.zeros(int(minutes/dt))
-    dicer = np.zeros(int(minutes/dt))
-    WT_dicer = np.zeros(int(minutes/dt))
-    short_dicer = np.zeros(int(minutes/dt))
-    mirna = np.zeros(int(minutes/dt))
+    dicer1 = np.zeros(int(minutes/dt))
+    dicer2 = np.zeros(int(minutes/dt))
+    WT_dicer1 = np.zeros(int(minutes/dt))
+    short_dicer2 = np.zeros(int(minutes/dt))
+    mirna1 = np.zeros(int(minutes/dt))
+    mirna2 = np.zeros(int(minutes/dt))
     
     #set init values
     WT[0] = WT_init
     short[0] = short_init
-    dicer[0] = dicer_init
+    dicer1[0] = dicer_init
+    dicer2[0] = dicer_init
     
     for i in range(1, int(minutes/dt)):
         if WT[i-1] < 0:
-            WT[i-1] = 0
-        if short[i-1] < 0:
-            short[i-1] = 0
-        if dicer[i-1] < 0:
-            pdicer[i-1] = 0
-        if WT_dicer[i-1] < 0:
-            WT_dicer[i-1]
-        if short_dicer[i-1] < 0:
-            short_dicer[i-1]
-        if mirna[i-1] < 0:
-            mirna[i-1] = 0
-            
-        WT[i] = WT[i-1] + dt * (WT_dicer[i-1] * k_1 - WT[i-1] * dicer[i-1] * k1)
-        short[i] = short[i-1] + dt * (short_dicer[i-1] * k_2 - short[i-1] * dicer[i-1] * k2)
-        dicer[i] = dicer[i-1] + dt * (WT_dicer[i-1] * (k_1 + k3) + short_dicer[i-1] * (k_2 + k3) - dicer[i-1] * (WT[i-1] * k1 + short[i-1] * k2))
-        WT_dicer[i] = WT_dicer[i-1] + dt * (WT[i-1] * dicer[i-1] * k1 - WT_dicer[i-1] * (k_1 + k3))
-        short_dicer[i] = short_dicer[i-1] + dt *(short[i-1] * dicer[i-1] * k2 - short_dicer[i-1] * (k_2 + k3))
-        mirna[i] = mirna[i-1] + dt * (k3 * (WT_dicer[i-1] + short_dicer[i-1]))
-    
-    return WT, short, dicer, WT_dicer, short_dicer, mirna
+            print(f'{i-1}: WT below 0: {WT[i-1]}')
+        elif short[i-1] < 0:
+            print(f'{i-1}: short below 0: {short[i-1]}')
+        elif dicer1[i-1] < 0:
+            print(f'{i-1}: dicer1 0: {dicer1[i-1]}')
+        elif dicer2[i-1] < 0:
+            print(f'{i-1}: dicer2 0: {dicer2[i-1]}')
+        elif WT_dicer1[i-1] < 0:
+            print(f'{i-1}: WT_dicer1 below 0: {WT_dicer1[i-1]}')
+        elif short_dicer2[i-1] < 0:
+            print(f'{i-1}: short_dice2r below 0: {short_dicer2[i-1]}')
+        elif mirna1[i-1] < 0:
+            print(f'{i-1}: mirna1 below 0: {mirna1[i-1]}')
+        elif mirna2[i-1] < 0:
+            print(f'{i-1}: mirna2 below 0: {mirna2[i-1]}')
+        
+        #wild type pre-mirna loop length
+        WT[i] = WT[i-1] * dt*(WT_dicer1[i-1]*k_1 - WT[i-1]*dicer1[i-1]*k1)
+        dicer1[i] = dicer1[i-1] * dt*(WT_dicer1[i-1]*(k_1 + k3) - dicer1[i-1] * WT[i-1] * k1)
+        mirna1 = (WT[0] + WT_dicer1[0]) - (WT_dicer1[i] + WT[i])
+        
+        #short loop pre-mirna
+        short[i] = short[i-1] * dt*(short_dicer2[i-1]*k_2 - short[i-1] * dicer2[i-1]*k2)
+        dicer2[i] = dicer2[i-1] * dt*(short_dicer2[i-1]*(k_2 * k3) - dicer2[i-1] * short[i-1] * k2)
+        mirna2 = (short[0] +  short_dicer2[0]) - (short[i] - short_dicer2[i])
+        
+        return WT, short 
 
-##Fraction diced
+
 def frac_diced(theta):
-    
-    WT, short, dicer, WT_dicer, short_dicer, mirna = conc_change(theta)
+    WT, short = conc_change(theta)
     
     WT_diced = np.zeros(int(minutes/dt))
     short_diced = np.zeros(int(minutes/dt))
@@ -99,8 +102,7 @@ def frac_diced(theta):
     
     return WT_diced, short_diced
 
-##Error function
-#create error function
+
 def error(theta):
     WT_diced, short_diced = frac_diced(theta)
     
@@ -113,3 +115,6 @@ def error(theta):
                             [short_d]])
     
     return np.sum((data_values - model_values ) ** 2)
+
+
+    
