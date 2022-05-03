cc66edb80c1c139aadd70d4694d83d1970dff3ac
SofiaRaak <sofia.raak@bristol.ac.uk>
2022-05-03 16:29:13 +0100

    Final commit of the day
    Tried a bunch of things, possible model overly flexible/stiff or
    singularity encountered after ~2.1 minutes.

--- .ipynb_checkpoints/test_model-checkpoint.py
+++ .ipynb_checkpoints/test_model-checkpoint.py
@@ -9,28 +9,31 @@ import math
 import params
 import tqdm
 
-def test_model():
-    WT, short = conc_change(model.theta)
-    for i in tqdm.tqdm(range(len(WT))):
-        assert type(WT[i]) is np.float64 
-        assert type(short[i]) is np.float64 
-        assert WT[i] >= 0 
-        assert short[i] >=0
-        assert WT[i] <= WT[0]
-        assert short[i] <= short[0]
+#def test_model():
+#    WT, short = conc_change(model.theta)
+#    for i in tqdm.tqdm(range(len(WT))):
+#        assert type(WT[i]) is np.float64 
+#        assert type(short[i]) is np.float64 
+#        assert WT[i] >= 0 
+#        assert short[i] >=0
+#        assert WT[i] <= WT[0]
+#        assert short[i] <= short[0]
         
 
-def test_fractions():
-    WT, short = frac_diced(model.theta)
-    for i in tqdm.tqdm(range(1, len(WT))):
-        assert WT[i] >= WT[i-1]
-        assert short[i] >= short[i-1]
+#def test_fractions():
+#    WT, short = frac_diced(model.theta)
+#    for i in tqdm.tqdm(range(1, len(WT))):
+#        assert WT[i] >= WT[i-1]
+#        assert short[i] >= short[i-1]
         
 def test_ODE_model():
-    sol = solve_ivp(ODE_model, (0, int(params.minutes)), model.init_values, t_eval = np.linspace(0, int(params.minutes), int(params.minutes/params.dt)))#, args = (model.theta))
+    sol = solve_ivp(ODE_model, (0, int(params.minutes)), model.init_values, t_eval = np.linspace(0, int(params.minutes), int(params.minutes/params.dt)))#, method = 'BDF')#, args = (model.theta))
     WT, WT_dicer, dicer1, mirna1, short, short_dicer, dicer2, mirna2 = sol.y
+    print(sol.message)
     for i in tqdm.tqdm(range(len(WT))):
         assert type(WT[i]) is np.float64
         assert type(short[i]) is np.float64 
         assert WT[i] >= 0 
         assert short[i] >=0
+        assert WT[i] <= WT[0]
+        assert short[i] <= short[0]
